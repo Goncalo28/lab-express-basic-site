fd80130e2035a12bd33f839c54a8af544015b045
Goncalo Marques <goncalomarques9@hotmail.com>
2020-11-02 20:36:22 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const { response } = require('express');
+const express = require('express');
+const { request } = require('http');
+const app = express();
+
+app.use(express.static('public'));
+
+//direct user to the /home page
+app.get('/', (request, response) => {
+    response.send('<a href="/home">Click to get to the Home Page</a>')
+})
+
+//home page
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname + '/views/home.html');
+});
+
+//works page
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + '/views/works.html');
+});
+
+//photo-gallery page
+app.get('/gallery', (request, response) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+//about page
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html');
+});
+
+
+app.listen(3000, () => {
+    console.log('App running on port 3000');
+});
